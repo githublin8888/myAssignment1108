43771eaea04f4b15296c358f381a27eadd85a7eb
githublin8888 <chunlinzhou2017@hotmail.com>
2022-11-19 21:21:17 +0100

    //refactor

--- src/assignment/Write.java
+++ src/assignment/Write.java
@@ -95,66 +95,6 @@ public class Write {
 
         }
     }
-    /*
-     public void rowNr(String text) {
-        if ("stop".equals(text)) {
-        } else {
-            row++;
-        }
-    }
-
-    public void characterNr(String text) {
-        if ("stop".equals(text)) {
-        } else {
-            length = length + text.replace(" ", "").length();
-        }
-    }
-
-    public void wordNr(String text) {
-        if ("stop".equals(text)) {
-        } else {
-            String[] wordArray = text.split("\\s");
-            int length2;
-            if (!text.equals("")) {
-                length2 = wordArray.length;
-
-            } else length2 = 0;
-
-            countWord = countWord + length2;
-
-        }
-    }
-
-    public void longWord(String text) {
-        if ("stop".equals(text)) {
-        } else {
-
-            text1=text1 + " " + text;;
-
-            String[] longArray = text1.split("\\s");
-
-            for (int i = 0; i < longArray.length - 1; i++) {
-                if (longArray[i].length() > longArray[i + 1].length()) {
-                    String temp = longArray[i];
-                    longArray[i] = longArray[i + 1];
-                    longArray[i + 1] = temp;
-                }
-                longestWord = longArray[longArray.length - 1];
-
-                int characterNr = longestWord.length();
-
-                for (int j = 0; j < longArray.length - 1; j++) {
-                    if (longArray[j].length() == characterNr) {
-                        longestWord = longestWord + ", " + longArray[j];
-                    }
-                }
-            }
-
-        }
-
-    }
-     */
-
 
 
 
